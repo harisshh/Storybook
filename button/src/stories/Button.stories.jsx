9df3f12616e5e5843cdac99c41a165ff50004296
Harish Sridhar <harishsridhar@Harishs-MacBook-Air.local>
2023-02-21 11:46:52 +0530

    Button Component Modified

--- button/src/stories/Button.stories.jsx
+++ button/src/stories/Button.stories.jsx
@@ -1,41 +1,35 @@
-import React from 'react';
+import React from 'react'
+import Button from './Button'
 
-import { Button } from './Button';
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'Form/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    // onclick: { action: 'clicked' },
-  },
-};
+  args: {
+    children: 'Button'
+  }
+}
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+export const Primary = () => <Button variant='primary'>Primary</Button>
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>
+export const Success = () => <Button variant='success'>Success</Button>
+export const Danger = () => <Button variant='danger'>Danger</Button>
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+const Template = args => <Button {...args} />
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+  variant: 'primary',
+  // children: 'Primary Args',
+}
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+  variant: 'secondary',
+  // children: 'Secondary Args',
+}
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  // children: 'Long Primary Args',
+}
